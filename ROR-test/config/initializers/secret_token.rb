a1d8fa51768454100431fc278eedc209614966b1
user <user@test.ru>
2012-10-08 19:49:36 +0400

    New ror

---
+++ ROR-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RORTest::Application.config.secret_token = 'a1d9216b49de29427d74ca12d2ede665cc6a7469c7711d11a3c3a0e952e338202fb695867ab58f2219945531ca24ca97208dc27819d2b0df9705e060b2468a89'
